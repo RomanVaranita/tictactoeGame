f79c75894e883128480436659456e3264b954557
RomanVaranita <romanvaranita@gmail.com>
2024-04-09 12:53:44 +0200

    add method printMap

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,25 @@ public class Main {
 
     static char[][] map;
 
+    static final int MAP_SIZE = 3;
+    static final char EMPTY_FIELD = '*';
+
     public static void main(String[] args) {
+        initMap();
+
+    }
+
+    public static void printMap(){
 
     }
 
     public static void initMap(){
-        map = new char[3][3];
+        map = new char[MAP_SIZE][MAP_SIZE];
+        for (int i = 0; i < MAP_SIZE; i++) {
+            for (int j = 0; j < MAP_SIZE; j++) {
+                map[i][j] = EMPTY_FIELD;
+            }
+        }
+
     }
 }
